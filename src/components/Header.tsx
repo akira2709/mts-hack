bea30c77c7cb3ba5866e6ca1bd6c96209ee6f0f2
akira2709 <145766714+akira2709@users.noreply.github.com>
2024-11-20 22:06:55 +0300

    stats page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {User} from "lucide-react";
 const Header = () => {
   return (
     <header className="border-b border-gray-800">
-      <div className="container mx-auto px-4 py-4">
+      <div className="py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-8">
             <img src='/img/logo.webp' alt='' className="h-[50px]" />
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -32,43 +32,48 @@ const Profile = () => {
   };
 
   return (
-    <div className="px-[80px]">
+    <div className="px-[20px] lg:px-[80px]">
       <Header />
-      <div className="min-h-screen flex items-center justify-center p-4">
-        <div className="w-full max-w-4xl bg-gray-800 rounded-2xl shadow-2xl overflow-hidden">
-          <div className="pt-20 px-8 pb-8">
-            <div className="flex justify-between items-start mb-8">
-              <div>
-                <h1 className="text-3xl font-bold text-white mb-2">{user.name}</h1>
-                <div className="flex gap-4 flex-wrap">
-                  {user.achievements.map((achievement, index) => (
-                    <div
-                      key={index}
-                      className="flex items-center gap-2 bg-gray-700 rounded-full px-4 py-1 transform transition hover:scale-105"
-                    >
-                      <achievement.icon className={`w-4 h-4 ${achievement.color}`} />
-                      <span className="text-sm text-gray-300">{achievement.label}</span>
-                    </div>
-                  ))}
-                </div>
-              </div>
+      <div className="flex flex-col lg:flex-row">
 
-              <div className="flex gap-6">
-                <StatCard icon={Film} label="Смотрит сейчас" value={user.stats.watching} />
-                <StatCard icon={Clock} label="Просмотрено" value={user.stats.completed} />
-              </div>
-            </div>
+	      <div className="h-fit flex lg:mt-4 w-full lg:w-[65vw]">
+	        <div className="w-full bg-gray-800 rounded-2xl shadow-2xl overflow-hidden lg:mr-4">
+	          <div className="pt-20 px-8 pb-8">
+	            <div className="flex flex-col lg:flex-row justify-between items-start mb-8">
+	              <div>
+	                <h1 className="text-3xl font-bold text-white mb-2">{user.name}</h1>
+	                <div className="flex gap-4 flex-wrap">
+	                  {user.achievements.map((achievement, index) => (
+	                    <div
+	                      key={index}
+	                      className="flex items-center gap-2 bg-gray-700 rounded-full px-4 py-1 transform transition hover:scale-105"
+	                    >
+	                      <achievement.icon className={`w-4 h-4 ${achievement.color}`} />
+	                      <span className="text-sm text-gray-300">{achievement.label}</span>
+	                    </div>
+	                  ))}
+	                </div>
+	              </div>
+	              <div className="flex gap-6 mt-6 lg:mt-0">
+	                <StatCard icon={Film} label="Смотрит сейчас" value={user.stats.watching} />
+	                <StatCard icon={Clock} label="Просмотрено" value={user.stats.completed} />
+	              </div>
+	            </div>
 
-            <div className="space-y-6">
-              <h2 className="text-xl font-semibold text-white">Любимые фильмы</h2>
-              <div className="grid grid-cols-3 gap-4">
-                {user.favoriteMovies.map((movie, index) => (
-                  <MovieCard type="По подписке" key={index} title={movie.title} rating={4} imageUrl={movie.poster}/>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+	            <div className="space-y-6">
+	              <h2 className="text-xl font-semibold text-white">Любимые фильмы</h2>
+	              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+	                {user.favoriteMovies.map((movie, index) => (
+	                  <MovieCard type="По подписке" key={index} title={movie.title} rating={4} imageUrl={movie.poster}/>
+	                ))}
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	      <div className="mt-4 bg-gray-800 flex-1 rounded-2xl p-4">
+					<LiderBoardCardList />
+				</div>
       </div>
     </div>
   );
@@ -84,6 +89,55 @@ function StatCard({ icon: Icon, label, value }: { icon: any, label: string, valu
       <div className="text-sm text-gray-400">{label}</div>
     </div>
   );
+}
+
+interface User {
+	username: string
+	points: number
+}
+
+function LiderBoardCardList() {
+	const cardList = [
+		{
+			username: "Павел Дуров",
+			points: 1000000
+		},
+		{
+			username: "Старый Бог",
+			points: 1537
+		},
+		{
+			username: "Seregga",
+			points: 1488
+		},
+		{
+			username: "Серега Пират",
+			points: 1337
+		},
+		{
+			username: "Колпакс Магомедович",
+			points: 228
+		},
+		{
+			username: "Язоло Раданович",
+			points: 52
+		},
+		{
+			username: "Головач Лена",
+			points: 42
+		},
+	]
+	return (
+		<>{cardList.map((user: User, index: number) =>
+				<div className="bg-gray-600 w-full h-12 rounded-lg mt-3 flex justify-between items-center px-4 font-['Helvetica']" key={index}>
+					<div className="flex gap-2">
+						<p className={index <= 2 ? "text-[#BA9D5A]" : ''}>{index + 1}</p>
+						<p>{ user.username }</p>
+					</div>
+					<p>{ user.points }</p>
+				</div>
+		)}</>
+	)
 }
 
 export default Profile;
